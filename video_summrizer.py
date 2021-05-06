47214e75bb9d04ed4208556c92605ff140afb39c
wangfei <1140554608@qq.com>
2021-05-05 21:14:03 -0700

    anti-mask & fix bug

--- video_summrizer.py
+++ video_summrizer.py
@@ -56,16 +56,25 @@ class VideoSummrizer():
                 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
                 matches = bf.match(last_des, curr_des)
                 matches_dist = np.array([x.distance for x in matches])
-
+                
                 self.frame_info[fid]['motion_feature_loc'] = [kp.pt for kp in curr_kp]
-                self.frame_info[fid]['motion_matched_num'] = len(matches)
+                
+                matched_num = np.sum(matches_dist < 60)
+                if len(curr_kp) < num_feature_points * 0.3 and len(last_kp) < num_feature_points * 0.3:
+                    self.frame_info[fid]['motion_matched_num'] = 1
+                else:
+                    self.frame_info[fid]['motion_matched_num'] = matched_num / (len(curr_kp) + len(last_kp) - matched_num)  # Jaccard Distance
+                    
                 # self.frame_info[fid]['motion_matched_dist'] = matches_dist
                 # self.frame_info[fid]['motion_max_dist'] = np.max(matches_dist)
                 # self.frame_info[fid]['motion_min_dist'] = np.min(matches_dist)
                 self.frame_info[fid]['motion_mean_dist'] = np.mean(matches_dist) if len(matches_dist) > 0 else 60
             except:
                 self.frame_info[fid]['motion_feature_loc'] = []
-                self.frame_info[fid]['motion_matched_num'] = 0
+                if len(curr_kp) < num_feature_points * 0.3 and len(last_kp) < num_feature_points * 0.3:
+                    self.frame_info[fid]['motion_matched_num'] = 1
+                else:
+                    self.frame_info[fid]['motion_matched_num'] = 0
                 self.frame_info[fid]['motion_mean_dist'] = 60
                 pass
 
@@ -104,7 +113,7 @@ class VideoSummrizer():
     def wave_detection(self):
         pass
 
-    def shot_segmentation(self, num_shots=60, min_frame_per_shot=5):
+    def shot_segmentation(self, num_shots=200, min_frame_per_shot=3):
         motion_matched_nums = [finfo['motion_matched_num'] for finfo in self.frame_info]
         scores = sorted(motion_matched_nums)
         diff_threshold = scores[num_shots]
@@ -113,7 +122,7 @@ class VideoSummrizer():
         for fid, finfo in enumerate(self.frame_info):
             if finfo['motion_matched_num'] <= diff_threshold:
                 shot_index.append(fid)
-
+        # print('key frames', shot_index)
         # merge short shots greedily
         # todo: short last shot 
         reduced_shot_index = []
@@ -124,19 +133,26 @@ class VideoSummrizer():
                 reduced_shot_index.append(shot_index[p])
         
         reduced_shot_index = [0] + reduced_shot_index + [len(self.img_array)]
-        
+        # print('shot spans', reduced_shot_index)
         self.shot_span = [(reduced_shot_index[i], reduced_shot_index[i+1]) for i in range(len(reduced_shot_index)-1)]
         
         return self.shot_span
     
-    def shot_selection(self, max_frame_per_shot=900, total_frame=2700):
+    def shot_selection(self, max_frame_per_shot=500, total_frame=2700):
         self.shot_score = [0 for _ in range(len(self.shot_span))]
-        for i, span in tqdm(enumerate(self.shot_span), desc='select shots'):
+        for sid, span in tqdm(enumerate(self.shot_span), desc='select shots'):
             score = 0
-            for j in range(span[0], span[1]):
-                score += int(self.frame_info[i]['face_num'] > 0) + self.frame_info[i]['motion_mean_dist'] / 60 + self.frame_info[i]['hist_diff']
-                # print(int(self.frame_info[i]['face_num'] > 0), self.frame_info[i]['motion_mean_dist'] / 367, self.frame_info[i]['hist_diff'])
-            self.shot_score[i] = score / (span[1] - span[0])
+            masked_frames = 0
+            for fid in range(span[0], span[1]):
+                # have_face; avg_motion_distance; color_distribution_difference; anti_mask
+                if len(self.frame_info[fid]['motion_feature_loc']) < 50:
+                    masked_frames += 1
+                score += 4.0 * int(self.frame_info[fid]['face_num'] > 0) + 3.0 * self.frame_info[fid]['motion_mean_dist'] / 60 + 2.0 * self.frame_info[fid]['hist_diff'] # + 5 * len(self.frame_info[i]['motion_feature_loc']) / 200
+            if masked_frames > min(15, (span[1] - span[0]) / 2.0 ):
+                self.shot_score[sid] = 0
+            else:
+                self.shot_score[sid] = score / (span[1] - span[0])
+            # print(sid, masked_frames)
         self.shot_score = np.array(self.shot_score)
         sorted_index = np.flip(np.argsort(self.shot_score))
 
@@ -149,7 +165,7 @@ class VideoSummrizer():
             selected_index.append(idx)
             total_frames += shot_frames
         selected_index = np.sort(np.array(selected_index))
-
+        # print('selected shots', selected_index)
         self.selected_shots = [self.shot_span[i] for i in selected_index]
         return self.selected_shots
         
@@ -193,7 +209,7 @@ class VideoSummrizer():
 
 if __name__ == '__main__':
     # preprocess video
-    path = '.\\project_dataset\\frames\\concert'
+    path = '.\\project_dataset\\frames\\test_video_2'
     img_array = []
     namelist = os.listdir(path)
     namelist = sorted(namelist, key = lambda x: int(x[5:-4]))
@@ -217,4 +233,5 @@ if __name__ == '__main__':
     video_summrizer.shot_selection()
     # export video
     video_summrizer.export_summrization()
+    video_summrizer.export_shots()
     video_summrizer.export_annotated_video()
